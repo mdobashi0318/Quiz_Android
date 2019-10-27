8086f0bfc6d43864b022c1ec73ecb07d717edead
Masaharu Dobashi <dobashi.masaharu@gmail.com>
2019-10-27 16:21:28 +0900

    クイズの新規追加処理を追加

--- app/src/main/java/com/example/quiz/QuizEdit.kt
+++ app/src/main/java/com/example/quiz/QuizEdit.kt
@@ -2,17 +2,58 @@ package com.example.quiz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import io.realm.Realm
+import io.realm.RealmResults
 import kotlinx.android.synthetic.main.activity_quiz_edit.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.yesButton
 
 class QuizEdit : AppCompatActivity() {
+
+
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_edit)
 
+        Realm.init(this)
+        realm = Realm.getDefaultInstance()
+
 
         editButton.setOnClickListener {
-            finish()
+
+
+            addRealm()
+
+
+            alert("クイズを作成しました") {
+                yesButton {
+                    finish()
+                }
+
+            }.show()
+
+
+        }
+    }
+
+
+
+
+    /// クイズを新規追加
+    private fun addRealm() {
+        realm.executeTransaction {
+            var id: Long = realm.where(QuizModel::class.java).count()
+            var quziModel = realm.createObject(QuizModel::class.java, id)
+            quziModel.quizTitle = titleEditText.text.toString()
+            quziModel.trueAnswer = trueEditText.text.toString()
+            quziModel.falseAnswer1 = false1EditText.text.toString()
+            quziModel.falseAnswer2 = false2EditText.text.toString()
+            quziModel.falseAnswer3 = false3EditText.text.toString()
+            quziModel.displayFlag = "0"
+
         }
     }
 }
--- app/src/main/java/com/example/quiz/QuizListActivity.kt
+++ app/src/main/java/com/example/quiz/QuizListActivity.kt
@@ -6,10 +6,35 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import io.realm.Realm
+import io.realm.RealmResults
+import kotlinx.android.synthetic.main.activity_quiz_list.*
 
 class QuizListActivity :  Fragment() {
 
+    private lateinit var realm: Realm
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.activity_quiz_list, container, false)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+
+        Realm.init(context)
+        realm = Realm.getDefaultInstance()
+        val quizModel: RealmResults<QuizModel> = realm.where(QuizModel::class.java).findAll()
+
+        var dataArray = mutableListOf<String>()
+
+        for (title in quizModel) {
+            dataArray.add(title.quizTitle)
+
+        }
+        val adapter = ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, dataArray)
+
+        quizArray.adapter = adapter
     }
 }
