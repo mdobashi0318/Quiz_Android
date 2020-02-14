87781c8530bc59001e4958c12782a487757c4d97
Masaharu Dobashi <dobashi.masaharu@gmail.com>
2020-02-14 23:06:14 +0900

    回答用のボタンにランダムでテキストをセットするよう更新

--- app/src/main/java/com/example/quiz/QuizScreenActivity.kt
+++ app/src/main/java/com/example/quiz/QuizScreenActivity.kt
@@ -3,6 +3,7 @@ package com.example.quiz
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_quiz_screen.*
 
 class QuizScreenActivity : AppCompatActivity() {
@@ -11,9 +12,34 @@ class QuizScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_screen)
 
+
         answer1.setOnClickListener {
             val intent = Intent(this.applicationContext, ResultScreenActivity::class.java)
             startActivity(intent)
         }
+
+        setAnswerText()
+
+    }
+
+
+
+
+    /// ボタンに回答をセットする
+    private fun setAnswerText() {
+
+        val buttons = mutableListOf(answer1, answer2, answer3, answer4)
+
+        val answerList = mutableListOf("問題1", "問題2", "問題3", "問題4")
+
+        for (i in 0 until buttons.count()) {
+
+            val num = (0 until answerList.count()).shuffled().first()
+            buttons[i].text = answerList[num]
+
+            answerList.removeAt(num)
+        }
+
+
     }
 }
