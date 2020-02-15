1c7b9de2a78d35065e76b1875aaa82e04e130e72
Masaharu Dobashi <dobashi.masaharu@gmail.com>
2020-02-15 17:10:39 +0900

    QuizScreenActivity 保存されたクイズのテキストをViewにセットする処理追加

--- app/src/main/java/com/example/quiz/QuizScreenActivity.kt
+++ app/src/main/java/com/example/quiz/QuizScreenActivity.kt
@@ -3,7 +3,8 @@ package com.example.quiz
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import io.realm.Realm
+import io.realm.RealmResults
 import kotlinx.android.synthetic.main.activity_quiz_screen.*
 
 class QuizScreenActivity : AppCompatActivity() {
@@ -12,13 +13,7 @@ class QuizScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_screen)
 
-
-        answer1.setOnClickListener {
-            val intent = Intent(this.applicationContext, ResultScreenActivity::class.java)
-            startActivity(intent)
-        }
-
-        setAnswerText()
+        setQuizText()
 
     }
 
@@ -26,11 +21,15 @@ class QuizScreenActivity : AppCompatActivity() {
 
 
     /// ボタンに回答をセットする
-    private fun setAnswerText() {
+    private fun setQuizText() {
 
         val buttons = mutableListOf(answer1, answer2, answer3, answer4)
 
-        val answerList = mutableListOf("問題1", "問題2", "問題3", "問題4")
+        val answers = getQuiz()
+
+        quizText.text = answers[0]
+
+        val answerList = mutableListOf(answers[1], answers[2], answers[3], answers[4])
 
         for (i in 0 until buttons.count()) {
 
@@ -38,8 +37,34 @@ class QuizScreenActivity : AppCompatActivity() {
             buttons[i].text = answerList[num]
 
             answerList.removeAt(num)
+
+
+            buttons[i].setOnClickListener {
+                val intent = Intent(this.applicationContext, ResultScreenActivity::class.java)
+                startActivity(intent)
+            }
         }
 
+    }
+
+
+
+
+
+    /// viewにセットするためテキストを取得する
+    private fun getQuiz(): MutableList<String?> {
+        Realm.init(applicationContext)
+        val realm = Realm.getDefaultInstance()
+        val quizModel: RealmResults<QuizModel> = realm.where(QuizModel::class.java).findAll()
+
+
+        return mutableListOf(
+            quizModel[0]?.quizTitle,
+            quizModel[0]?.trueAnswer,
+            quizModel[0]?.falseAnswer1,
+            quizModel[0]?.falseAnswer2,
+            quizModel[0]?.falseAnswer3
+        )
 
     }
 }
